414921a592dad83b700af460dd59db4a5a1fd783
maxxfuu <maxfu826@gmail.com>
2025-07-09 13:13:17 -0700

    edit navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,7 +2,7 @@ import { FolderCode, PenLine, Sparkles, SquareMousePointer } from 'lucide-react'
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center py-3 sm:py-4 border-b border-[#252525] text-[#9CA3AF] sticky top-0 bg-black z-50 min-h-[50px] sm:min-h-[60px]">
+    <div className="flex justify-between items-center py-4 border-b border-[#252525] text-[#9CA3AF] sticky top-0 bg-black z-50 min-h-[50px] sm:min-h-[60px]">
       {/* Left Nav */}
       <div className="flex items-center px-2 sm:px-4">
         <p className="text-sm sm:text-base font-medium">maxxfuu</p>
@@ -16,11 +16,7 @@ export default function Navbar() {
         </li>
         <li className="flex items-center text-xs sm:text-sm gap-1 sm:gap-2 cursor-pointer hover:text-white transition-colors">
           <Sparkles size={14} className="sm:w-4 sm:h-4"/>
-          <a href="#" className="hidden sm:inline">experience</a>
-        </li>
-        <li className="flex items-center text-xs sm:text-sm gap-1 sm:gap-2 cursor-pointer hover:text-white transition-colors">
-          <FolderCode size={14} className="sm:w-4 sm:h-4"/>
-          <a href="#" className="hidden sm:inline">projects</a>
+          <a href="#" className="hidden sm:inline">gems</a>
         </li>
         <li className="flex items-center text-xs sm:text-sm gap-1 sm:gap-2 cursor-pointer hover:text-white transition-colors">
           <PenLine size={14} className="sm:w-4 sm:h-4"/>
