24280e0a3d09ccb221dcd8094260b0673b605d36
maxxfuu <maxfu826@gmail.com>
2025-07-13 23:54:46 -0700

    fix build

--- src/app/writing/on-becoming-an-effective-engineer.tsx/page.tsx
+++ src/app/writing/on-becoming-an-effective-engineer.tsx/page.tsx
@@ -3,7 +3,7 @@ export default function ReflectionsOnCS() {
     <div>
       <h1 className="text-lg flex justify-left items-center mt-10 text-white font-bold">On becoming an effective engineer</h1>
       <p className="text-md flex justify-center items-center mt-5">
-        Working as an intern I've realized 2 traits that makes a effective engineer. Taking ownership, the ability to take building a additional feature personally. Understanding the full depth of the business model, the ability to understand the business objectives which is beyond the scope of technical work.
+        Working as an intern I&apos;ve realized 2 traits that makes a effective engineer. Taking ownership, the ability to take building a additional feature personally. Understanding the full depth of the business model, the ability to understand the business objectives which is beyond the scope of technical work.
       </p>
     </div>
   )
