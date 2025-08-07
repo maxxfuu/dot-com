1117458b1a2439febdd4756a2a0fc44583e99af5
maxxfuu <maxfu826@gmail.com>
2025-08-06 17:24:10 -0700

    fixed formatting for blogs on mobile

--- src/app/writing/blog1.tsx/page.tsx
+++ src/app/writing/blog1.tsx/page.tsx
@@ -1,13 +1,13 @@
 export default function ReflectionsOnCS() {
   return (
-    <div>
+    <div className="px-6">
       <h1 className="text-lg flex justify-left items-center mt-10 text-white font-bold">To be a generalist or a specialist</h1>
-      <p className="text-md flex justify-center items-center mt-5">
+      <p className="text-md flex justify-left items-center mt-5">
         A thought experiment on the trade-off between being a generalist or a specialist and why I think being a generalist is the better choice.
       </p>
       <br />
       <p>
-        
+
       </p>
     </div>
   )
